eedb2864f9e1571c0f621a8caaa31ec5955bedc8
Anto Sergeevich <Fokkuszim08@mail.ru>
2023-01-30 17:53:01 +0300

    first commit

---
+++ Zadasha_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+ if (a > b)
+ 
+   {
+ 
+    Console.WriteLine("{0} большее число", a);
+    }
+   else
+    { 
+    Console.WriteLine("{0} большее число ", b);
+    } 
+ 
---
+++ Zadasha_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(Math.Max(a, b), c);
+int min = Math.Min(Math.Min(a, b), c);
+Console.WriteLine(max);
