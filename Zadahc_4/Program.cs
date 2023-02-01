c83a3ee653dff7f35bd47485bd2d040cf164add0
Anto Sergeevich <Fokkuszim08@mail.ru>
2023-02-01 13:48:38 +0300

    g

---
+++ Zadahc_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// static void Main(string[] args);
+// Console.WriteLine("Введате любое число: ");
+// int m = int.Parse(Console.ReadLine());
+// int n = 1;
+// for (int i = n; i <= m; i++)
+// {
+//     if (i % 2 == 0) 
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
