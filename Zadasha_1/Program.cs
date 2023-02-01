e5f83b53394bae9557146f6cb39c80aa99005831
Anto Sergeevich <Fokkuszim08@mail.ru>
2023-02-01 16:42:28 +0300

    g

--- Zadasha_1/Program.cs
+++ Zadasha_1/Program.cs
@@ -1,16 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Введите число a ");
-int a = Convert.ToInt32(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число b ");
-int b = Convert.ToInt32(Console.ReadLine());
- if (a > b)
- 
-   {
- 
-    Console.WriteLine("{0} большее число", a);
-    }
-   else
-    { 
-    Console.WriteLine("{0} большее число ", b);
-    } 
- 
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
