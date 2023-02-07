6a1e59b8c806dd1e7c7f9787ce309e1378b0f2e3
Anton Sergeevich <Fokkuszim08@mail.ru>
2023-02-07 21:53:14 +0300

    Vnesenu pravki2

--- Zadasha_1/Program.cs
+++ Zadasha_1/Program.cs
@@ -6,5 +6,4 @@ int b = int.Parse(Console.ReadLine()!);
 int max = a;
 if (a > max) max = a;
 if (b > max) max = b;
-Console.Write("max = ");
-Console.WriteLine(max);
+Console.WriteLine($"max = {(max)}");
