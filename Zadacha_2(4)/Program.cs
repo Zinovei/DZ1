fd24e5f5baf5e0f8627b388e31ca58abd47d48c0
Anton Sergeevich <Fokkuszim08@mail.ru>
2023-02-08 17:29:06 +0300

    g

--- Zadacha_2(4)/Program.cs
+++ Zadacha_2(4)/Program.cs
@@ -10,4 +10,4 @@ if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 Console.Write("max = ");
-Console.WriteLine(max);
+Console.WriteLine(max); 
--- Zadasha_1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Введите число a ");
-int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число b ");
-int b = int.Parse(Console.ReadLine()!);
-int max = a;
-if (a > max) max = a;
-if (b > max) max = b;
-Console.WriteLine($"max = {(max)}");
